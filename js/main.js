ec7df7ba4248c80c3e8fa195d213147cbd3e6e62
jpedrogall@gmail.com <jpedrogall@gmail.com>
2024-05-14 16:35:19 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('#Telefone').mask('(00) 00000-0000', {
+        placeholder: '(  ) '
+    })
+    $('#CPF').mask('00000000000', {
+        placeholder: '000.000.000-00'
+    })
+    $('#CEP').mask('00000000', {
+        placeholder: '00000-000'
+    })
+})
