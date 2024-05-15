c153dd6a0f667578f78fdc2379392ebb1525e522
jpedrogall@gmail.com <jpedrogall@gmail.com>
2024-05-15 11:17:33 -0300

    fix mask

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@ $(document).ready(function(){
     $('#Telefone').mask('(00) 00000-0000', {
         placeholder: '(  ) '
     })
-    $('#CPF').mask('00000000000', {
+    $('#CPF').mask('000000000 00', {
         placeholder: '000.000.000-00'
     })
-    $('#CEP').mask('00000000', {
+    $('#CEP').mask('00000 000', {
         placeholder: '00000-000'
     })
 })
